496f664c7c929afbd3ceee126c35fbd0899925eb
Jules Damji <dmatrix@comcast.net>
2021-07-28 18:58:24 -0700

    Added module1/readme.md and modified some files
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_1/feature_repo/entities/entity.py
+++ module_1/feature_repo/entities/entity.py
@@ -4,4 +4,4 @@ import feast as fst
 
 # Define an entity for the driver. You can think of entity as a primary key used to
 # fetch features.
-driver = fst.Entity(name="driver_id", value_type=fst.ValueType.INT64, description="driver id",)
+driver = fst.Entity(name="driver_id", value_type=fst.ValueType.INT64, description="driver id")
