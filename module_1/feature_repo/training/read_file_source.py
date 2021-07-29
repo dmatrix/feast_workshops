866ee493ab925ec308594badcc3e23dc69a18c26
Jules Damji <dmatrix@comcast.net>
2021-07-29 11:07:45 -0700

    Added scoring step and how to read the parquet file
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ module_1/feature_repo/training/read_file_source.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    DATA_SOURCE_FILE="/Users/jules/git-repos/feast_workshops/module_1/feature_repo/data/driver_stats.parquet"
+    df = pd.read_parquet(DATA_SOURCE_FILE)
+    pd.set_option('display.max_columns', None)
+    print(df.head(2))
+    print(f"Data Source Parquet Shape={df.shape}")
--- module_1/feature_repo/training/scoring.py
+++ module_1/feature_repo/training/scoring.py
@@ -2,7 +2,7 @@ from pprint import pprint
 from fsutils import get_online_data
 
 if __name__ == "__main__":
-    FEAST_REPO = "/Users/jules/git-repos/feature_repo"
+    FEAST_REPO = "/Users/jules/git-repos/feast_workshops/module_1/feature_repo"
     drivers = [1001, 1002, 1003]
     for driver in drivers :
         feature_vector = get_online_data(FEAST_REPO, driver)
