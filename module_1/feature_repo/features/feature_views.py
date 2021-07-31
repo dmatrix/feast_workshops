93f04dd418da78074ffd7304799de10c8bcc999c
Jules Damji <dmatrix@comcast.net>
2021-07-31 13:52:22 -0700

    removed fst imports and added READMEs for module2 and 3
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_1/feature_repo/features/feature_views.py
+++ module_1/feature_repo/features/feature_views.py
@@ -1,6 +1,5 @@
 # This is an example feature definition file
-import feast as fst
-
+from feast import FeatureView, Feature, ValueType
 from datasources.filesource import driver_hourly_stats
 
 from google.protobuf.duration_pb2 import Duration
@@ -8,14 +7,14 @@ from google.protobuf.duration_pb2 import Duration
 # Our parquet files contain sample data that includes a driver_id column, timestamps and
 # three feature column. Here we define a Feature View that will allow us to serve this
 # data to our model online.
-driver_hourly_stats_view = fst.FeatureView(
+driver_hourly_stats_view = FeatureView(
     name="driver_hourly_stats",
     entities=["driver_id"],
     ttl=Duration(seconds=86400 * 1),
     features=[
-        fst.Feature(name="conv_rate", dtype=fst.ValueType.FLOAT),
-        fst.Feature(name="acc_rate", dtype=fst.ValueType.FLOAT),
-        fst.Feature(name="avg_daily_trips", dtype=fst.ValueType.INT64),
+        Feature(name="conv_rate", dtype=ValueType.FLOAT),
+        Feature(name="acc_rate", dtype=ValueType.FLOAT),
+        Feature(name="avg_daily_trips", dtype=ValueType.INT64),
     ],
     online=True,
     input=driver_hourly_stats,
