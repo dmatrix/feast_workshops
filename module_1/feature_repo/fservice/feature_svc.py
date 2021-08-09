1a16846c3264ab97c1da5e3ada2e8ed4c1d372c9
Jules Damji <dmatrix@comcast.net>
2021-08-09 13:58:26 -0700

    Added description as a tag
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_1/feature_repo/fservice/feature_svc.py
+++ module_1/feature_repo/fservice/feature_svc.py
@@ -3,4 +3,6 @@ from features.feature_views import driver_hourly_stats_view
 
 # Define your feature service and the features it will serve
 driver_fs = FeatureService(name="driver_ranking_fv_svc",
-                           features=[driver_hourly_stats_view])
+                           features=[driver_hourly_stats_view],
+                           tags={"description": "Used for training an ElasticNetCV model"})
+
