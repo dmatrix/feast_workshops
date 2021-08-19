fc5b25ff5b5a0479019a165940f3cd21465d21c9
Jules Damji <dmatrix@comcast.net>
2021-08-19 16:03:31 -0700

    added some comments
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_1/feature_repo/datasource/file_source.py
+++ module_1/feature_repo/datasource/file_source.py
@@ -1,11 +1,10 @@
 # This is an example feature definition file
 from feast import FileSource
 
-# Read data from parquet files. Parquet is convenient for local development mode. For
-# production, you can use your favorite DWH, such as BigQuery. See Feast documentation
-# for more info.
-driver_hourly_stats = FileSource(
-    path="/Users/jules/git-repos/feature_repo/data/driver_stats.parquet",
-    event_timestamp_column="datetime",
-    created_timestamp_column="created",
-)
+# Read data from parquet files. Parquet is convenient for local development mode. For production, you can 
+# use your favorite data warehouse (DWH), such as BigQuery or AWS Redshift. See Feast documentation for more info.
+
+# change path to your location in the repo
+driver_hourly_stats = FileSource(path="/Users/jules/git-repos/feature_repo/data/driver_stats.parquet",
+                                 event_timestamp_column="datetime",
+                                 created_timestamp_column="created")
--- module_1/feature_repo/entities/entity.py
+++ module_1/feature_repo/entities/entity.py
@@ -2,11 +2,6 @@
 
 from feast import Entity, ValueType
 
-# Read data from parquet files. Parquet is convenient for local development mode. For
-# production, you can use your favorite DWH, such as BigQuery. See Feast documentation
-# for more info.
-
-
 # Define an entity for the driver. You can think of entity as a primary key used to
 # fetch features.
 driver = Entity(name="driver_id", value_type=ValueType.INT64, description="driver id",)
