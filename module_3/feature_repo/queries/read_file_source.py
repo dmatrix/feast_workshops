e2866696f8b59040f0cb212635764d40b13ee797
Jules Damji <dmatrix@comcast.net>
2021-09-07 18:26:09 -0700

    Added lab jupyter notebooks
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_3/feature_repo/queries/read_file_source.py
+++ module_3/feature_repo/queries/read_file_source.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 # Change path to your repo
 ZIPCODE_TABLE = "/Users/jules/git-repos/feast_workshops/module_3/feature_repo/data/zipcode_table.parquet"
-LOANS_TABLE =  "/Users/jules/git-repos/feast_workshops/module_3/feature_repo/data/loan_table.parquet"
+LOANS_TABLE = "/Users/jules/git-repos/feast_workshops/module_3/feature_repo/data/loan_table.parquet"
+CREDIT_HISTORY_TABLE="/Users/jules/git-repos/feast_workshops/module_3/feature_repo/data/credit_history.parquet"
 
 if __name__ == "__main__":
-    for file in [ZIPCODE_TABLE,LOANS_TABLE]:
+    for file in [ZIPCODE_TABLE, LOANS_TABLE, CREDIT_HISTORY_TABLE]:
         df = pd.read_parquet(file)
         pd.set_option('display.max_columns', 15)
         print(f"File: {file}")
---
+++ module_3/feature_repo/queries/train_model.py
@@ -0,0 +1,5 @@
+
+class XGBoostClassifier:
+
+    def __init__(self):
+        pass
