dd6ab10ca67b290348a38b6693469a6ba9f490f4
Jules Damji <dmatrix@comcast.net>
2021-09-22 11:33:07 -0700

    Fix minor bug in datasource and add comments to change to your git location
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- module_1/feature_repo/queries/read_file_source.py
+++ module_1/feature_repo/queries/read_file_source.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    DATA_SOURCE_FILE = "/Users/jules/git-repos/feast_workshops/module_1/feature_repo/data/driver_stats.parquet"
+    DATA_SOURCE_FILE = "/Users/jsd/git-repos/feast_workshops/module_1/feature_repo/data/driver_stats.parquet"
     df = pd.read_parquet(DATA_SOURCE_FILE)
     pd.set_option('display.max_columns', 10)
     print(df.head(2))
